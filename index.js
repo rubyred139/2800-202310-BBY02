3fda22cd4d29f3057bacef1d102a91edcf5ee85b
Victor Fung <98victorfung@gmail.com>
2023-05-09 11:34:37 -0700

    added mongoDB connection to index.js

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+require('dotenv').config();
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
 
 const Joi = require("joi");
 
-const session = require('express-session');
-
-require('dotenv').config();
-
 const port = process.env.PORT || 5000;
+
+const mongodb_host = process.env.MONGODB_HOST;
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_database = process.env.MONGODB_DATABASE;
+
+var {database} = include('databaseConnection');
+
+const userCollection = database.db(mongodb_database).collection('countries');
 
 app.set('view engine', 'ejs');
 
@@ -18,7 +25,6 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.send("Hello ADVENTOURs");
 });
-
 
 
 app.use(express.static(__dirname + "/public"));
