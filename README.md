91569d452874f14333f85839635cbae0f72fd2af
Victor Fung <98victorfung@gmail.com>
2023-05-09 10:23:08 -0700

    updated dev1 to match main

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 ## General Info
 Our team is developing an app called AdvenTour to help travellers discover unique and authentic experiences in places experiencing under-tourism using AI.
+
+We will be using a dataset from Kaggle.
+
+Install:
+-express
+-express-session
+-dotenv
+-connect-mongo
+-bcrypt
+-joi
+-ejs
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+
+const app = express();
+
+const Joi = require("joi");
+
+const session = require('express-session');
+
+require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get('/', (req, res) => {
+    res.send("Hello ADVENTOURs");
+});
+
+
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("*", (req, res) => {
+    res.status(404);
+    res.send("Page not found - 404");
+})
+
+app.listen(port, () => {
+    console.log("Node application listening on port " + port);
+}); 
