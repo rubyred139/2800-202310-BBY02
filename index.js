6074a2d07265d5bb6c4de7103d9fdcdb158c8413
Victor Fung <98victorfung@gmail.com>
2023-05-10 10:01:22 -0700

    added sessions to database

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require("./utils.js");
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const bcrypt = require('bcrypt');
 
 const app = express();
 
@@ -14,6 +16,9 @@ const mongodb_host = process.env.MONGODB_HOST;
 const mongodb_user = process.env.MONGODB_USER;
 const mongodb_password = process.env.MONGODB_PASSWORD;
 const mongodb_database = process.env.MONGODB_DATABASE;
+const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
+
+const node_session_secret = process.env.NODE_SESSION_SECRET;
 
 var {database} = include('databaseConnection');
 
@@ -22,6 +27,21 @@ const userCollection = database.db(mongodb_database).collection('countries');
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
+
+var mongoStore = MongoStore.create({
+	mongoUrl: `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_host}/sessions`,
+	crypto: {
+		secret: mongodb_session_secret
+	}
+})
+
+app.use(session({ 
+    secret: node_session_secret,
+	store: mongoStore,
+	saveUninitialized: false, 
+	resave: true
+}
+));
 
 
 app.get('/', (req, res) => {
