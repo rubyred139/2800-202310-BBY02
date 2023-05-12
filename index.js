8c4e5966929645cadb606d8e9690277cd0d909af
Ruby <rchen139@my.bcit.ca>
2023-05-11 20:23:08 -0700

    resolve conflict

--- index.js
+++ index.js
@@ -35,15 +35,6 @@ var {database} = include('databaseConnection');
 
 const untrvl_countries = database.db(mongodb_database).collection('under-travelled_countries');
  
-
-const mongodb_host = process.env.MONGODB_HOST;
-const mongodb_user = process.env.MONGODB_USER;
-const mongodb_password = process.env.MONGODB_PASSWORD;
-const mongodb_database = process.env.MONGODB_DATABASE;
-const mongodb_session_secret = process.env.MONGODB_SESSION_SECRET;
-
-const node_session_secret = process.env.NODE_SESSION_SECRET;
-
 var {database} = include('databaseConnection');
 
 const userCollection = database.db(mongodb_database).collection('countries');
