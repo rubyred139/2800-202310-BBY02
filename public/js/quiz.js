ee2098947ad34adf0f367248677b2f78de32a90e
Gaganjit Heer <heergaganjit@gmail.com>
2023-05-25 11:29:03 -0700

    Fixed bug with reset button on quiz page

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,46 +1,41 @@
-const optionBtns = document.querySelectorAll('.option-btn');
+let optionBtns = document.querySelectorAll('.option-btn');
 const activeBtns = {};
 
-optionBtns.forEach((btn) => {
-  const question = btn.getAttribute('data-question');
+function initializeButtons() {
+  optionBtns.forEach((btn) => {
+    const question = btn.getAttribute('data-question');
 
-  // Add active class to the first option button by default
-  if (!activeBtns[question]) {
-    btn.classList.add('active');
-    activeBtns[question] = btn;
-    const value = btn.getAttribute('data-value');
-    const input = document.querySelector(`#${question}-input`);
-    input.value = value;
-  }
-
-  btn.addEventListener('click', (e) => {
-    const value = e.target.getAttribute('data-value');
-    const input = document.querySelector(`#${question}-input`);
-
-    // Remove active class from previously active button for this question
-    if (activeBtns[question]) {
-      activeBtns[question].classList.remove('active');
+    // Add active class to the first option button by default
+    if (!activeBtns[question]) {
+      btn.classList.add('active');
+      activeBtns[question] = btn;
+      const value = btn.getAttribute('data-value');
+      const input = document.querySelector(`#${question}-input`);
+      input.value = value;
     }
 
-    // Add active class to newly selected button and update input value
-    e.target.classList.add('active');
-    activeBtns[question] = e.target;
-    input.value = value;
+    btn.addEventListener('click', (e) => {
+      const value = e.target.getAttribute('data-value');
+      const input = document.querySelector(`#${question}-input`);
+
+      // Remove active class from previously active button for this question
+      if (activeBtns[question]) {
+        activeBtns[question].classList.remove('active');
+      }
+
+      // Add active class to newly selected button and update input value
+      e.target.classList.add('active');
+      activeBtns[question] = e.target;
+      input.value = value;
+    });
   });
-});
+}
+
+initializeButtons(); // Call the function to initialize buttons initially
 
 // Reset function
 function resetAnswers() {
-  // Clear input values
-  const inputs = document.querySelectorAll('input[type="hidden"]');
-  inputs.forEach((input) => {
-    input.value = '';
-  });
-
-  // Remove active class from all option buttons
-  optionBtns.forEach((btn) => {
-    btn.classList.remove('active');
-  });
+  location.reload(); // Reload the page
 }
 
 // Event listener for the reset button
