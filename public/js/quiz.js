9db709a9ffb386bf315708e2e31be1204822af68
Gaganjit Heer <heergaganjit@gmail.com>
2023-05-25 03:02:56 -0700

    Added asterisks for required fields and clear button on quiz

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,22 +1,48 @@
+const optionBtns = document.querySelectorAll('.option-btn');
+const activeBtns = {};
 
-  const optionBtns = document.querySelectorAll('.option-btn');
-  const activeBtns = {};
+optionBtns.forEach((btn) => {
+  const question = btn.getAttribute('data-question');
 
-  optionBtns.forEach((btn) => {
-    const question = btn.getAttribute('data-question');
+  // Add active class to the first option button by default
+  if (!activeBtns[question]) {
+    btn.classList.add('active');
+    activeBtns[question] = btn;
+    const value = btn.getAttribute('data-value');
+    const input = document.querySelector(`#${question}-input`);
+    input.value = value;
+  }
 
-    btn.addEventListener('click', (e) => {
-      const value = e.target.getAttribute('data-value');
-      const input = document.querySelector(`#${question}-input`);
+  btn.addEventListener('click', (e) => {
+    const value = e.target.getAttribute('data-value');
+    const input = document.querySelector(`#${question}-input`);
 
-      // Remove active class from previously active button for this question
-      if (activeBtns[question]) {
-        activeBtns[question].classList.remove('active');
-      }
+    // Remove active class from previously active button for this question
+    if (activeBtns[question]) {
+      activeBtns[question].classList.remove('active');
+    }
 
-      // Add active class to newly selected button and update input value
-      e.target.classList.add('active');
-      activeBtns[question] = e.target;
-      input.value = value;
-    });
+    // Add active class to newly selected button and update input value
+    e.target.classList.add('active');
+    activeBtns[question] = e.target;
+    input.value = value;
   });
+});
+
+// Reset function
+function resetAnswers() {
+  // Clear input values
+  const inputs = document.querySelectorAll('input[type="hidden"]');
+  inputs.forEach((input) => {
+    input.value = '';
+  });
+
+  // Remove active class from all option buttons
+  optionBtns.forEach((btn) => {
+    btn.classList.remove('active');
+  });
+}
+
+// Event listener for the reset button
+const resetBtn = document.querySelector('#reset-btn');
+resetBtn.addEventListener('click', resetAnswers);
